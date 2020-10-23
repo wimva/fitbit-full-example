370affb3c4c1d3dcb457f195fff00ab032cdd6c3
Wim Van Aerschot <wim@bagaar.be>
2020-10-23 12:22:57 +0200

    added comments on navigation

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -2,22 +2,28 @@ import document from 'document';
 
 let pages = {};
 
+// initialize navigation
 export function init(router) {
   pages = router;
 }
 
+// get current page
 export function getPage() {
   const re = /.*\/+(.*)+\..*/;
   return document.location.pathname.replace(re, '$1');
 }
 
+// when page unloads (swipe back)
 export function onUnload() {
   const page = getPage();
   if (pages[page] && pages[page].destroy) pages[page].destroy();
 }
 
+// navigate to another page
 export async function switchPage(nextPage, stack) {
   const pagePath = `./resources/pages/${nextPage}.view`;
+
+  // when page is not the same as current page
   if (pagePath !== document.location.pathname) {
     const page = getPage();
     if (stack) {
@@ -26,10 +32,16 @@ export async function switchPage(nextPage, stack) {
       await document.location.replace(pagePath);
     }
 
+    // set swipe back handler
     document.onbeforeunload = onUnload;
 
+    // destroy previous page
     if (!stack && pages[page] && pages[page].destroy) pages[page].destroy();
+
+    // make sure to destroy new page first so we start off from an empty slate
     if (pages[nextPage] && pages[nextPage].destroy) pages[nextPage].destroy();
+
+    // start up new page
     if (pages[nextPage] && pages[nextPage].init) pages[nextPage].init();
   }
 }

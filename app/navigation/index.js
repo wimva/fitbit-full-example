6827e26cff8577ebd3590c030e688561d0e2e3b5
Wim Van Aerschot <>
2021-10-15 09:28:37 +0200

    added 4.x

--- app/navigation/index.js
+++ app/navigation/index.js
@@ -1,45 +1,49 @@
 import document from 'document';
 
 let pages = {};
+let previousPage = 'index';
 
 // De navigatie initialiseren met de router (zie router.js)
 export function init(router) {
   pages = router;
 }
 
-// De naam van de huidige view ophalen.
+// De naam van de huidige pagina ophalen.
 export function getPage() {
   const re = /.*\/+(.*)+\..*/;
   return document.location.pathname.replace(re, '$1');
 }
 
-// Functie wordt hieronder aangeroepen. Nodig voor het terugswipen.
-export function onUnload() {
-  const page = getPage();
-  if (pages[page] && pages[page].destroy) pages[page].destroy();
+// Button presses opvangen
+function onKeyPress(e) {
+  switch (e.key) {
+    default:
+      console.log('unknown key pressed');
+      break;
+    case 'back':
+      e.preventDefault();
+      switchPage(previousPage);
+      break;
+  }
 }
 
-export async function switchPage(nextPage, stack) {
-  const pagePath = `./resources/pages/${nextPage}.view`;
+// Naar een nieuwe pagina navigeren
+export function switchPage(nextPage) {
+  const pagePath = `./resources/pages/${nextPage}.gui`;
 
   // Controleren of de geselecteerde pagina niet huidige pagina is.
   if (pagePath !== document.location.pathname) {
-    if (stack) {
-      await document.location.assign(pagePath);
-    } else {
-      await document.location.replace(pagePath);
+    const page = getPage();
+    document.replaceSync(pagePath);
+    if (nextPage !== 'index') {
+      previousPage = page;
+      document.onkeypress = onKeyPress;
     }
 
-    // De huidige pagina in een constante steken zodat we deze in de functies hieronder kunnen gebruiken.
-    const page = getPage();
+    // De vorige pagina vernietigen
+    if (pages[page] && pages[page].destroy) pages[page].destroy();
 
-    // Event om te weten wanneer er wordt teruggeswiped.
-    document.onbeforeunload = onUnload;
-
-    // De vorige pagina vernietigen.
-    if (!stack && pages[page] && pages[page].destroy) pages[page].destroy();
-
-    // Een nieuwe pagina starten.
+    // Een nieuwe pagina starten
     if (pages[nextPage] && pages[nextPage].init) pages[nextPage].init();
   }
 }
